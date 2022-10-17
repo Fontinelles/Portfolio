eac1dce9ade9d417c357d1f99a9abaa6186e2cd7
PEDRO HENRIQUE FONTINELLES BEZERRA <pedro.bezerra@fnde.gov.br>
2022-10-17 13:52:24 -0300

    novo link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
       <SocialNetworks />
       <InformationContainer />
       <a
-        href="https://api8w.ilovepdf.com/v1/download/22x3dAyfpp1ktlqd043smw5s63rpmAxyq022ht4xvzpgAy76l95zr5vm6yw27mAvbzc5t2j43681db9Atn8xgAs794097vt4m3661bytzs54hprcc4znfmcsyx825z6dl83hAr1yk8y8kfldj5zcy1nn68mm15j7nqbg0ks8sApp8Aqr89fq"
+        href="https://drive.google.com/file/d/1zt4oumAKOI9T8ABJP_EWgVDtBqRgH3e9/view?usp=sharing"
         className="btn"
       >
         Download currículo
